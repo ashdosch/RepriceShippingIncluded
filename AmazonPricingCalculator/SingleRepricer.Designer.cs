1c68da2c0b1784d28160cb3da0d4fa7f66122384
Ashley Dosch <AshleyD@delmarfans.com>
2018-03-20 14:50:27 -0400

    Included non-template logic

--- AmazonPricingCalculator/SingleRepricer.Designer.cs
+++ AmazonPricingCalculator/SingleRepricer.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AmazonPricingCalculator
 {
-    partial class Form1
+    partial class SingleRepricer
     {
         /// <summary>
         /// Required designer variable.
@@ -41,6 +41,12 @@
             this.text_PM = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // contextMenuStrip1
@@ -50,9 +56,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(267, 352);
+            this.button1.Location = new System.Drawing.Point(122, 180);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(236, 113);
+            this.button1.Size = new System.Drawing.Size(176, 46);
             this.button1.TabIndex = 1;
             this.button1.Text = "Calculate";
             this.button1.UseVisualStyleBackColor = true;
@@ -60,21 +66,21 @@
             // 
             // text_DN
             // 
-            this.text_DN.Location = new System.Drawing.Point(306, 152);
+            this.text_DN.Location = new System.Drawing.Point(147, 38);
             this.text_DN.Name = "text_DN";
             this.text_DN.Size = new System.Drawing.Size(151, 20);
             this.text_DN.TabIndex = 2;
             // 
             // text_Shipping
             // 
-            this.text_Shipping.Location = new System.Drawing.Point(305, 188);
+            this.text_Shipping.Location = new System.Drawing.Point(146, 64);
             this.text_Shipping.Name = "text_Shipping";
             this.text_Shipping.Size = new System.Drawing.Size(152, 20);
             this.text_Shipping.TabIndex = 3;
             // 
             // text_Comm
             // 
-            this.text_Comm.Location = new System.Drawing.Point(305, 228);
+            this.text_Comm.Location = new System.Drawing.Point(146, 90);
             this.text_Comm.Name = "text_Comm";
             this.text_Comm.Size = new System.Drawing.Size(152, 20);
             this.text_Comm.TabIndex = 4;
@@ -82,7 +88,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(239, 155);
+            this.label1.Location = new System.Drawing.Point(84, 41);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(61, 13);
             this.label1.TabIndex = 5;
@@ -91,16 +97,16 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(225, 191);
+            this.label2.Location = new System.Drawing.Point(7, 67);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.Size = new System.Drawing.Size(138, 13);
             this.label2.TabIndex = 6;
-            this.label2.Text = "Shipping Cost:";
+            this.label2.Text = "Shipping Cost OR FBA Fee:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(224, 231);
+            this.label3.Location = new System.Drawing.Point(69, 93);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(76, 13);
             this.label3.TabIndex = 7;
@@ -109,7 +115,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(192, 274);
+            this.label4.Location = new System.Drawing.Point(37, 119);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(108, 13);
             this.label4.TabIndex = 8;
@@ -117,7 +123,7 @@
             // 
             // text_PM
             // 
-            this.text_PM.Location = new System.Drawing.Point(306, 271);
+            this.text_PM.Location = new System.Drawing.Point(146, 116);
             this.text_PM.Name = "text_PM";
             this.text_PM.Size = new System.Drawing.Size(152, 20);
             this.text_PM.TabIndex = 9;
@@ -125,11 +131,11 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(352, 90);
+            this.label5.Location = new System.Drawing.Point(144, 9);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.Size = new System.Drawing.Size(102, 13);
             this.label5.TabIndex = 10;
-            this.label5.Text = "Repricer";
+            this.label5.Text = "Single Item Repricer";
             // 
             // label6
             // 
@@ -143,11 +149,74 @@
             this.label6.TabIndex = 11;
             this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
-            // Form1
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(65, 289);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(80, 13);
+            this.label13.TabIndex = 18;
+            this.label13.Text = "Loop Iterations:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(111, 243);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Price:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Main-Products-ShippingAddedByTemplate",
+            "Accessories-ShippingAddedByTemplate"});
+            this.comboBox1.Location = new System.Drawing.Point(146, 142);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(244, 21);
+            this.comboBox1.TabIndex = 27;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(47, 145);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(98, 13);
+            this.label12.TabIndex = 28;
+            this.label12.Text = "Shipping Template:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(114, 314);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(194, 43);
+            this.button3.TabIndex = 32;
+            this.button3.Text = "CLEAR ALL";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(70, 267);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(75, 13);
+            this.label8.TabIndex = 33;
+            this.label8.Text = "Shipping Cost:";
+            // 
+            // SingleRepricer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(810, 563);
+            this.ClientSize = new System.Drawing.Size(421, 378);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label13);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.text_PM);
@@ -159,8 +228,8 @@
             this.Controls.Add(this.text_Shipping);
             this.Controls.Add(this.text_DN);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "SingleRepricer";
+            this.Text = "SingleRepricer";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -180,6 +249,12 @@
         private System.Windows.Forms.TextBox text_PM;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label8;
     }
 }
 
---
+++ AmazonPricingCalculator/SingleRepricer.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AmazonPricingCalculator
+{
+    public partial class SingleRepricer : Form
+    {
+        public SingleRepricer()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double adjusted_dn = Double.Parse(text_DN.Text);
+            double commInput = Double.Parse(text_Comm.Text);
+            double shipping = 0;
+            if (!string.IsNullOrEmpty(text_Shipping.Text)) { shipping = Double.Parse(text_Shipping.Text); }
+            double profitMarginInput = Double.Parse(text_PM.Text);
+            label7.ForeColor = Color.Black;
+            label7.Text = "Price:";
+
+            if (comboBox1.Text != "" && shipping != 0)
+            {
+                label7.ForeColor = Color.Red; 
+                label7.Text = "Please only enter a shipping template OR a shipping cost!";
+            }
+            else
+            {
+                double commPercentage = commInput / 100;
+                double profitMargin = profitMarginInput / 100;
+                double PM_Lower_Range = profitMargin;
+                double PM_Upper_Range = profitMargin + .0004;
+
+                if (shipping > 0)
+                {
+                    double tempTotal = adjusted_dn + shipping;
+                    double commission = tempTotal * commPercentage;
+                    double total = (tempTotal + commission);
+                    double price = total + (total * .25);
+                    double PM = (price - total) / price;
+                    int count = 0;
+
+                    while ((PM < PM_Lower_Range) || (PM > PM_Upper_Range))
+                    {
+                        label13.Text = "Loop Iterations: " + count;
+                        if (PM > PM_Upper_Range)
+                        {
+                            price = price - .01;
+                            commission = Math.Round((price * commPercentage), 2);
+                            total = tempTotal + commission;
+                            PM = (price - total) / price;
+                        }
+                        else if (PM < PM_Lower_Range)
+                        {
+                            price = price + .01;
+                            commission = Math.Round((price * commPercentage), 2);
+                            total = tempTotal + commission;
+                            PM = (price - total) / price;
+                        }
+                        count++;
+                        price = Math.Round(price, 2);
+                    }
+
+                    price = Math.Round(price, 2);
+                    label7.Text = "Price: " + price;
+                    label8.Text = "Shipping Cost: " + shipping;
+                }
+                else
+                {
+                    double commission2 = adjusted_dn * commPercentage;
+                    double total2 = (adjusted_dn + commission2);
+                    string templateType = comboBox1.Text;
+                    double currShipping = template_values(total2, templateType);
+                    total2 = total2 + currShipping;
+                    double price2 = total2 + (total2 * .25);
+                    double PM = (price2 - total2) / price2;
+
+                    int count = 0;
+
+                    while ((PM < PM_Lower_Range) || (PM > PM_Upper_Range))
+                    {
+                        label13.Text = "Loop Iterations: " + count;
+                        if (PM > PM_Upper_Range)
+                        {
+                            price2 = price2 - .01;
+                            commission2 = Math.Round((price2 * commPercentage), 2);
+                            shipping = template_values(price2, templateType);
+                            total2 = commission2 + adjusted_dn + shipping;
+                            PM = (price2 - total2) / price2;
+                        }
+                        else if (PM < PM_Lower_Range)
+                        {
+                            price2 = price2 + .01;
+                            commission2 = Math.Round((price2 * commPercentage), 2);
+                            shipping = template_values(price2, templateType);
+                            total2 = commission2 + adjusted_dn + shipping;
+                            PM = (price2 - total2) / price2;
+                        }
+                        count++;
+                        price2 = Math.Round(price2, 2);
+                    }
+
+                    price2 = Math.Round(price2, 2);
+                    label7.Text = "Price: " + price2;
+                    label8.Text = "Shipping Cost: " + shipping;
+                }
+            }
+        }
+
+        private double template_values(double price, string template)
+        {
+            if (template == "Main-Products-ShippingAddedByTemplate")
+            {
+                if (price <= 10) { return 7.99; }
+                else if (price > 10 && price <= 20) { return 8.99; }
+                else if (price > 20 && price <= 30) { return 10.99; }
+                else { return 12.99; }
+            }
+            else
+            {
+                if (price <= 10) { return 4.99; }
+                else if (price > 10 && price <= 20) { return 6.99; }
+                else if (price > 20 && price <= 30) { return 8.99; }
+                else { return 10.99; }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label7.ForeColor = Color.Black;
+            label7.Text = "Price:";
+            text_DN.Text = "";
+            text_Comm.Text = "";
+            text_Shipping.Text = "";
+            text_PM.Text = "";
+            comboBox1.Text = "";
+            label13.Text = "Loop Iterations:";
+            label8.Text = "Shipping Cost:";
+        }
+        
+    }
+}
